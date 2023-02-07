08f786068731fe47bfcf50f8fd3687d90a33bc8e
Luís Henrique <luishsilva09@gmail.com>
2023-02-07 11:11:16 -0300

    fix: nome de variaveis

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -64,32 +64,32 @@ class UserService {
     //tamanho do cpf
     if (cpf.length != 11) return false;
 
-    let soma1 = 0;
-    let soma2 = 0;
+    let soma_1 = 0;
+    let soma_2 = 0;
     let cont = 2;
-    let dig1 = 0;
-    let dig2 = 0;
+    let dig_1 = 0;
+    let dig_2 = 0;
 
     //Descrobrir primeiro digito
     for (let i = 8; i >= 0; i--) {
       let res = Number(cpf[i]) * cont;
-      soma1 += res;
+      soma_1 += res;
       cont++;
     }
-    if (soma1 % 11 >= 2) dig1 = 11 - (soma1 % 11);
+    if (soma_1 % 11 >= 2) dig_1 = 11 - (soma_1 % 11);
 
     //reinicia contador
     cont = 2;
     //Descobri segundo digito
     for (let i = 9; i >= 0; i--) {
       let res = Number(cpf[i]) * cont;
-      soma2 += res;
+      soma_2 += res;
       cont++;
     }
-    if (soma2 % 11 >= 2) dig2 = 11 - (soma2 % 11);
+    if (soma_2 % 11 >= 2) dig_2 = 11 - (soma_2 % 11);
 
     //verifia se os digitos sao iguais
-    if (String(dig1) + String(dig2) === cpf[9] + cpf[10]) {
+    if (String(dig_1) + String(dig_2) === cpf[9] + cpf[10]) {
       return true;
     } else {
       return false;
